b58285dd11d325374ba358b162f3a0b6c1ff5c7a
Vic Wang <vicwang95@gmail.com>
2023-02-19 11:35:40 -0500

    update

--- Package.swift
+++ Package.swift
@@ -29,8 +29,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .binaryTarget(
             name: "SDWebImage",
-            path: "build/SDWebImage.xcframework"
+            url: "https://github.com/bluepi0j/SDWebImage/releases/download/1.0/SDWebImage.xcframework.zip",
+            checksum: ""
         ),
+//        .binaryTarget(
+//            name: "SDWebImage",
+//            path: "build/SDWebImage.xcframework"
+//        ),
 //        .target(
 //            name: "SDWebImage",
 //            dependencies: [],
